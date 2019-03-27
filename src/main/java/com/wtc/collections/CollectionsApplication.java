529825863268d05e5f4ac0b7fa48f86f8d6ecf45
吴天成 <975930271@qq.com>
2019-03-27 09:52:36 +0800

    初始化

---
+++ src/main/java/com/wtc/collections/CollectionsApplication.java
@@ -0,0 +1,13 @@
+package com.wtc.collections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CollectionsApplication.class, args);
+    }
+
+}
